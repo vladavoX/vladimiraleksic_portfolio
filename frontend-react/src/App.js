df7e55f31757daef9f56ec8f56a98038accd73d9
Vladimir Aleksic <vlada.pzs@hotmail.com>
2022-03-09 23:29:52 +0100

    importing components to the app.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
 
+import { About, Footer, Header, Skills, Testimonials, Work } from './container'
+import { Navbar } from './components'
+import './App.scss'
+
 const App = () => (
   <div className="app">
-
+    <Navbar />
+    <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonials />
+    <Footer />
   </div>
 );
 
